b690b10f3b1848573ecab50730987c29e9ddf1aa
GreenWyvern <gamevingw@gmail.com>
2018-11-07 22:24:17 -0500

    Make Database, Added ConnectionClass and Example Query

---
+++ App_Code/Carinfo.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+/// <summary>
+/// Summary description for info
+/// </summary>
+public class Carinfo
+{
+    public int carID { get; set; }
+    public int garageID { get; set; }
+    public string model { get; set; }
+    public string make { get; set; }
+    public string milage { get; set; }
+    public string engine { get; set; }
+    public string condition { get; set; }
+
+    public Carinfo(int carID, int garageID, string model, string make, string milage, string engine, string condition)
+    {
+
+    }
+}
