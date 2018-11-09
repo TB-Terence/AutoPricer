9fa40b0050caefdb2a23cb780b8c7f6c87552692
micheal sun <michealzun@gmail.com>
2018-11-09 18:28:46 -0500

    yuyi listing page update

--- Listing_Result.aspx.cs
+++ Listing_Result.aspx.cs
@@ -7,8 +7,13 @@ using System.Web.UI.WebControls;
 
 public partial class Listing_Result : System.Web.UI.Page
 {
+    private int listingId;
+    private string userName;
+    private int carId;
+    private string suggestedPrice;
+    private string listingStatus;
     protected void Page_Load(object sender, EventArgs e)
     {
-
+        
     }
 }
